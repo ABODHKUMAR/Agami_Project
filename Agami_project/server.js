f35ac558e3198671b5f1aed7114e985508d13508
abodh5921 <abodh5921@gmail.com>
2024-02-05 11:20:13 +0530

    DB Connect Works

--- Agami_project/server.js
+++ Agami_project/server.js
@@ -1,11 +1,28 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000  
 
 
+const connectDB = async()=>{
+    try{
+        const conn = await mongoose.connect('mongodb://localhost:27017/Agami_project');
+        console.log( `Connected TO Mongodb ${conn.connection.host}`
+        );
+    }
+    catch(error){
+        console.log(`Error in Mongodb ${error}`)
+    }
+}
 
+//Connect to MongoDb
+connectDB();
+
+//Routes
+
+
+//server is works
 app.listen(PORT ,()=>{
     console.log(`Server is runing on port ${PORT}`);
 })
